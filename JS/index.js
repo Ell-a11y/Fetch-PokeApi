b17bded4cbd206cc8381bbeac97c6df317000ec5
Erik <erik.llorens.espin@gmail.com>
2026-01-16 16:58:42 +0100

    Falta la informacion extra del card

--- JS/index.js
+++ JS/index.js
@@ -2,57 +2,61 @@
 const contenedor = document.getElementById("contenedor-pokes");
 const select = document.querySelector("select");
 const pagSig = document.querySelector("#sigPag");
-const button = document.getElementById("Shiny");
+const pagAnt = document.querySelector("#antPag");
 // Variables LET
 let pokemon = null;
-let shiny = true;
-let valorSelect = Number(select.value);
-let numPagina = valorSelect;
+let numPagina = 1;
 
 // FUNCIONES ANONIMAS CONST
-const creadorObj = function(obj, bool) {
+const creadorObj = function(obj) {
     const divNew = document.createElement("article");
     const imgNew = document.createElement("img");
     const pNew = document.createElement("p");
     pNew.textContent = obj.id + ". " + obj.name;
     imgNew.src = obj.sprites.front_default;
-    if(!bool) {
-        imgNew.src = obj.sprites.front_shiny;
-    }
     divNew.classList = ("pokemons");
     contenedor.appendChild(divNew);
     divNew.appendChild(imgNew);
     divNew.appendChild(pNew);
 }
-const genPokes = async function(num, bool) {
+const genPokes = async function() {
     contenedor.textContent = "";
-    for(let x = 1; x<=num; x++) {
-        await obtenerPoke(x, bool);
+    for(let x = numPagina; x<Number(select.value)+numPagina; x++) {
+        await obtenerPoke(x);
     }
 }
-const convShiny = function(num, bool) {
-    shiny = !shiny;
-    genPokes(num, bool);
-}
-const sigPagina = async function(num, bool) {
+const sigPagina = async function(num) {
     contenedor.textContent = "";
-    for(let x = 1; x<=num; x++) {
-        numPagina++;
-        await obtenerPoke(numPagina, bool);
+    numPagina = Number(numPagina)+Number(num);
+    for(let x = numPagina; x<numPagina+num; x++) {
+        await obtenerPoke(x);
     }
-
+}
+const antPagina = async function(num) {
+    contenedor.textContent = "";
+    if(numPagina > Number(select.value) || Number(numPagina)-Number(num) >= 1) {
+        console.log(numPagina)
+        for(let x = numPagina-num; x<numPagina; x++) {
+            await obtenerPoke(x);
+        }
+        numPagina = Number(numPagina)-Number(num);
+    } else {
+        for(let x = 1; x <= num; x++) {
+            await obtenerPoke(x);
+        }
+    }
 }
 
 // Promesas FETCH
-const obtenerPoke = async function(id, bool) {
+const obtenerPoke = async function(id) {
     const tmp = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const obj = await tmp.json();
-    creadorObj(obj, bool);
+    creadorObj(obj);
 }
 // AddEventListener
-select.addEventListener("change", ()=>genPokes(valorSelect, shiny));
-button.addEventListener("click", ()=>convShiny(valorSelect, shiny));
-pagSig.addEventListener("click", ()=>sigPagina(valorSelect, shiny));
+select.addEventListener("change", genPokes);
+pagSig.addEventListener("click", ()=>sigPagina(Number(select.value)));
+pagAnt.addEventListener("click", ()=>antPagina(Number(select.value)));
 
 // Funcions automaticas
-genPokes(select.value, shiny);
+genPokes(select.value, false);
